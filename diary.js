238c2a297e00662535df314b4e5340d6986b8e03
Egdeliya Nurgalieva <e.nurgalieva@evrone.com>
2024-10-15 18:20:25 +0300

    add an array of quill editors

--- diary.js
+++ diary.js
@@ -1,60 +1,63 @@
-const entryBody = document.querySelector('.entry-body');
 const pageContainer = document.querySelector('.page-container');
 
-const entryTitleEditor = new Quill('#entry-title', {
-    theme: 'bubble',
-    placeholder: 'Entry title',
+const entryTitleEditor = new Quill('#entry-title', {theme: 'bubble', placeholder: 'Entry title'});
 
-});
-const entryBodyEditor = new Quill('#entry-body', {
+let pageEditors = [];
+let currentPageEditor = new Quill('#page-0', {
     theme: 'bubble'
 });
+pageEditors.push(currentPageEditor);
 
-let prevHeight = entryBody.offsetHeight;
+let currentPageId = 0;
 
-entryBodyEditor.on('text-change', (delta, oldDelta, source) => {
-    if (source == 'api') {
-        console.log('An API call triggered this change.');
-    } else if (source == 'user') {
-        console.log('A user action triggered this change.');
-    }
-    console.log("old change ---> ", oldDelta);
+let currentPage = document.querySelector('.page-0');
+let initialHeight = currentPage.offsetHeight;
 
-    // TODO maybe detect overflow with y-coordinates
-    if (entryBody.offsetHeight <= prevHeight) {
+currentPageEditor.on('text-change', addNewPage);
+
+// Quill event handler on 'text-change'
+function addNewPage(delta, oldDelta, source) {
+    if (currentPage.offsetHeight <= initialHeight) {
         return;
     }
 
-    console.log("-------------> here we have an overflow!")
+    console.log("-------------> here we have an overflow! so there is a need to create a new page")
     console.log("current change ---> ", delta);
 
-    entryBodyEditor.disable();
+    pageEditors[currentPageId].disable();
 
-    newPage = createNewPage();
-    pageContainer.appendChild(newPage);
+    currentPageId++;
 
-    let newPageId = `#new-page-${currentPageId}`
-    // TODO create an array of page editors
-    const newPageEditor = new Quill(newPageId, {
+    let blankPage = createBlankPage(currentPageId);
+    pageContainer.appendChild(blankPage);
+
+    // TODO use editor.addContainer after moving to the first blank page
+    const blankPageEditor = new Quill(`#page-${currentPageId}`, {
         theme: 'bubble'
     });
+    pageEditors.push(blankPageEditor);
 
+    // TODO think of a more clever way to detect overflow
     let overflowStopper = document.createElement('div');
     overflowStopper.classList.add('overflow-stopper');
     overflowStopper.tabIndex = -1;
     // overflowStopper.hidden = true;
 
-    newPage.appendChild(overflowStopper);
-});
+    blankPage.appendChild(overflowStopper);
+    blankPageEditor.focus();
 
-let currentPageId = 0;
+    currentPage = document.querySelector(`#page-${currentPageId}`);
+    initialHeight = currentPage.offsetHeight;
 
-function createNewPage() {
+    currentPageEditor = pageEditors[currentPageId];
+    currentPageEditor.on('text-change', addNewPage)
+}
+
+function createBlankPage(pageId) {
     let newPage = document.createElement('div');
-    newPage.classList.add('new-page');
+    newPage.classList.add('blank-page');
 
-    currentPageId++;
-    newPage.id = `new-page-${currentPageId}`;
+    newPage.id = `page-${pageId}`;
 
     // TODO move overflowing text to the next page
     // newPage.textContent = text;
