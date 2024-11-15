f68df34070d9098ab20441b75ba04934d28ab233
Egdeliya Nurgalieva <e.nurgalieva@evrone.com>
2024-11-15 18:23:50 +0100

    add search_all html page

--- diary.js
+++ diary.js
@@ -1,5 +1,7 @@
 const pageContainer = document.querySelector('.page-container');
 
+// TODO думала, что js файл будет отрабатывать 1 раз при загрузке всех html страниц, но это не так.
+// в итоге для search_all не находится элемент с id entry-title
 const entryTitleEditor = new Quill('#entry-title', {theme: 'bubble', placeholder: 'Entry title'});
 
 let pageEditor = new Quill('#page-0', {
