179abf67fe8c67c1328a724d7ac6042b6fedf400
Egdeliya Nurgalieva <e.nurgalieva@evrone.com>
2025-01-10 20:52:39 +0100

    store entries in local storage

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # mini-diary
 
+A journal app focused on simplicity. Users can create, edit using the Quill text editor, 
+read, and search diary entries. All data is stored locally on the user’s device. 
+The minimalistic design helps users focus on writing without distractions.
+
+Tested on MacOS. 
+## Dependencies
+- npm
+[Install npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+
+- electron
+```bash
+npm install electron --save-dev
+```
+
 ## Copyright
-Copyright © 2024 Egdeliia Nurgalieva. See [license](LICENSE) for details.
+Copyright © 2024 Egdeliia Samirova. See [license](LICENSE) for details.
---
+++ new_entry.js
@@ -0,0 +1,47 @@
+dateElement = document.getElementsByClassName('date')[0];
+let today = new Date();
+let opts = {day: "numeric", month: "long", year: "numeric"};
+dateElement.innerText = today.toLocaleString("ru-RU", opts);
+
+let localStorage = window.localStorage;
+
+const entryTitleEditor = new Quill('#entry-title', {
+    theme: 'bubble',
+    placeholder: 'Entry title'
+});
+
+function updateTitle(delta, oldDelta, source) {
+    if (source === 'api') {
+        return;
+    }
+
+    localStorage.setItem(`current-entry-title`, entryTitleEditor.getText());
+}
+
+entryTitleEditor.on('text-change', updateTitle);
+
+let entryBodyEditor = new Quill('#page-0', {
+    theme: 'bubble',
+    placeholder: 'Entry body'
+});
+
+function updatePage(delta, oldDelta, source) {
+    if (source === 'api') {
+        return;
+    }
+
+    localStorage.setItem(`current-entry`, entryBodyEditor.getText());
+}
+
+entryBodyEditor.on('text-change', updatePage);
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+}
+
+searchEntriesLink = document.getElementById("search-all");
+searchEntriesLink.addEventListener("click", (e) => {
+    // save current entry to file / sqlite
+})
