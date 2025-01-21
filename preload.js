c6254600bff6913614c3a5149a1e9bf6eff10c4d
Egdeliya Nurgalieva <e.nurgalieva@evrone.com>
2025-01-21 17:17:40 +0100

    add full text search

--- preload.js
+++ preload.js
@@ -2,6 +2,18 @@ const { contextBridge, ipcRenderer } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
+const { Document } = require('flexsearch');
+
+const indexOptions = {
+    charset: "cyrillic",
+    tokenize: "forward",
+    document: {
+        id: "uid",
+        index: ["title", "content"]
+    }
+}
+const index = new Document(indexOptions);
+
 const diaryFilePath = path.join(__dirname, 'diary.json');
 
 function loadEntries() {
@@ -15,10 +27,39 @@ function loadEntries() {
 function saveEntry(uid, entry) {
     const entries = loadEntries();
     entries[uid] = entry;
+    if (index.contain(uid)) {
+        index.update(uid, entry);
+    } else {
+        index.add(uid, entry);
+    }
     fs.writeFileSync(diaryFilePath, JSON.stringify(entries, null, 2));
+}
+
+function buildIndex(entries) {
+    for (let uid in entries) {
+        if (index.contain(uid)) {
+            break;
+        }
+        index.add(entries[uid]);
+    }
+}
+
+function search(pattern) {
+    let searchObj = index.search(pattern);
+    let uidsSet = new Set();
+    for (let i in searchObj) {
+        let result = searchObj[i].result;
+        for (let j in result) {
+            uidsSet.add(result[j]);
+        }
+    }
+
+    return uidsSet;
 }
 
 contextBridge.exposeInMainWorld('diaryAPI', {
     loadEntries: () => loadEntries(),
     saveEntry: (uid, entry) => saveEntry(uid, entry),
+    buildIndex: (entries) => buildIndex(entries),
+    search: (pattern) => search(pattern),
 });
--- search_entries.js
+++ search_entries.js
@@ -1,10 +1,12 @@
 let entries = window.diaryAPI.loadEntries();
+window.diaryAPI.buildIndex(entries);
 
 let previews = document.getElementsByClassName("entries-list")[0];
 
 for (let uid in entries) {
     const entry = entries[uid];
     const li = document.createElement('li');
+    li.setAttribute('data-uid', uid);
     li.innerHTML = `
     <article class="entry-preview">
         <div>
@@ -32,5 +34,25 @@ for (let uid in entries) {
     `;
     previews.appendChild(li);
 }
+
+let searchInput = document.getElementById("search");
+
+searchInput.addEventListener("keyup", e => {
+    let pattern = searchInput.value.toLowerCase();
+
+    const foundUids = window.diaryAPI.search(pattern);
+
+    let listElements = previews.getElementsByTagName("li");
+    for (let i = 0; i < listElements.length; i++) {
+        let elem = listElements[i];
+        let uid = elem.getAttribute("data-uid");
+
+        if (foundUids.has(uid) || pattern === "") {
+            elem.removeAttribute("hidden");
+        } else {
+            elem.setAttribute("hidden", "");
+        }
+    }
+});
 
 
