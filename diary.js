112b5581d55a4c75e0ad566636288a5055349ca2
Egdeliia Samirova <egdeliya@mail.ru>
2025-03-21 18:42:43 +0100

    add quill editor

---
+++ diary.js
@@ -0,0 +1,8 @@
+const entryTitleEditor = new Quill('#entry-title', {
+    theme: 'bubble',
+    placeholder: 'Entry title',
+
+})
+const entryBodyEditor = new Quill('#entry-body', {
+    theme: 'bubble'
+});
