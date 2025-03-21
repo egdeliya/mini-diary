90cf27d324a85ab63d8a6d23307306c213c32761
Egdeliia Samirova <egdeliya@mail.ru>
2025-03-21 18:42:43 +0100

    create overlapping div in page overflow

--- diary.js
+++ diary.js
@@ -1,3 +1,6 @@
+const entryBody = document.querySelector('.entry-body');
+const pageContainer = document.querySelector('.page-container');
+
 const entryTitleEditor = new Quill('#entry-title', {
     theme: 'bubble',
     placeholder: 'Entry title',
@@ -6,3 +9,31 @@ const entryTitleEditor = new Quill('#entry-title', {
 const entryBodyEditor = new Quill('#entry-body', {
     theme: 'bubble'
 });
+
+let prevHeight = entryBody.offsetHeight;
+
+entryBodyEditor.on('text-change', (delta, oldDelta, source) => {
+    if (source == 'api') {
+        console.log('An API call triggered this change.');
+    } else if (source == 'user') {
+        console.log('A user action triggered this change.');
+    }
+    console.log("old change ---> ", oldDelta);
+
+    if (entryBody.offsetHeight <= prevHeight) {
+        return;
+    }
+
+    console.log("-------------> here we have an overflow!")
+    console.log("current change ---> ", delta);
+
+    newPage = createNewPage()
+    pageContainer.appendChild(newPage);
+});
+
+function createNewPage() {
+    let newPage = document.createElement('div');
+    newPage.classList.add('new-page');
+    // newPage.textContent = text;
+    return newPage;
+}
