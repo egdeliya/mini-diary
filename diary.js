7a83b6b20c2f52feddaa21d4f64a1c0f8bf23a90
Egdeliia Samirova <egdeliya@mail.ru>
2025-03-21 18:42:43 +0100

    make hamburger menu with :target pseudo class

--- diary.js
+++ diary.js
@@ -6,30 +6,3 @@ const entryTitleEditor = new Quill('#entry-title', {
 const entryBodyEditor = new Quill('#entry-body', {
     theme: 'bubble'
 });
-
-// hide menu if back button clicked
-const backBtn = document.getElementsByClassName("back")[0];
-const navItems = document.getElementsByClassName("nav-items")[0];
-const menuCheckbox = document.querySelector("#menu-checkbox");
-const menuBtn = document.querySelector(".menu-btn");
-console.log(menuCheckbox)
-console.log(menuCheckbox.checked);
-
-menuCheckbox.addEventListener("change", (e) => {
-    console.log(e)
-    console.log(menuCheckbox.checked);
-
-    navItems.style.visibility = "visible";
-    menuBtn.style.display = "none";
-})
-
-backBtn.addEventListener("click", () => {
-    console.log("click!")
-    console.log(navItems)
-    console.log(menuCheckbox.checked)
-
-    navItems.style.visibility = "hidden";
-
-    menuBtn.style.display = "block";
-    menuCheckbox.checked = false;
-})
